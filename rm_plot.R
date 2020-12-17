9ca3ded852dc2bd7422c2cefd8af25eb79e8d1e5
Francisco Yira <francisco.yira@outlook.com>
2020-12-17 14:09:53 -0300

    change in week definition (so each week starts on monday) and change in number of tests used for prediction (so it is congruent with plot captions)

--- rm_plot.R
+++ rm_plot.R
@@ -28,7 +28,7 @@ examenes_casos <-
   examenes_region_dia %>% 
   left_join(casos_region_dia,
             by = c("Region", "Fecha")) %>% 
-  mutate(Semana = lubridate::week(Fecha))
+  mutate(Semana = lubridate::isoweek(Fecha))
 
 
 examenes_casos_rm <- 
@@ -51,18 +51,18 @@ median_tests <-
   median()
 
 # Nest by week and run regressions
-casos_pred_median_testing <- 
+casos_pred_10000_tests <- 
   examenes_casos_rm %>% 
   nest(data = c(-Semana)) %>% 
   mutate(reg_casos = map(data, ~lm(N_Casos ~ N_Tests, data = .)),
-         pred_with_median_tests = map_dbl(reg_casos, ~predict(., newdata = tibble(N_Tests = median_tests))))
+         pred_with_median_tests = map_dbl(reg_casos, ~predict(., newdata = tibble(N_Tests = 10000))))
 
 # First plot
 examenes_casos_rm %>% 
   filter(Semana %in% c(31, 35, 40, 49)) %>% 
   ggplot(aes(N_Tests, N_Casos, color = factor(Semana))) +
   geom_point() +
-  geom_smooth(method = "lm") +
+  geom_smooth(method = "lm", fullrange = TRUE) +
   labs(
     subtitle = "Data for Metropolitan Region, Chile",
     x = "Number of Tests",
@@ -72,30 +72,33 @@ examenes_casos_rm %>%
 # Second plot
 examenes_casos_rm %>% 
   filter(Semana %in% c(31, 35, 40, 49)) %>% 
-  left_join(casos_pred_median_testing,
+  left_join(casos_pred_10000_tests,
             by = "Semana") %>% 
   ggplot(aes(N_Tests, N_Casos, color = factor(Semana))) +
   geom_point() +
-  geom_vline(xintercept = median_tests, color = "blue4") +
-  geom_point(aes(x = median_tests, y = pred_with_median_tests), size = 5,
+  geom_vline(xintercept = 10000, color = "blue4") +
+  geom_point(aes(x = 10000, y = pred_with_median_tests), size = 5,
              show.legend = FALSE) +
-  geom_smooth(method = "lm") +
+  geom_smooth(method = "lm",
+              fullrange = TRUE,
+              se = FALSE) +
   labs(
     subtitle = "Data for Metropolitan Region, Chile",
     x = "Number of Tests",
     y = "Number of Cases",
     color = "Week") +
   annotate("label",
-           x = median_tests*0.91,
-           y = 900,
-           label = "Median of daily tests",
+           x = 10000*0.95,
+           y = 1000,
+           label = "10.000 daily tests",
            color = "blue4")
 
 # Function to create date from week number
 week_to_date <- function(week_n) {
-  as.Date(str_c("2020", week_n, "1"), "%Y%U%u") %>% 
-    magrittr::subtract(1) %>% 
-    format("%B %d",
+  week_start <- 
+    as.Date(str_c("2020", week_n, "1"), "%Y%U%u") %>% 
+    magrittr::subtract(7) %>% 
+    format("%b %d",
            locale = locale(date_names = "en"))
   
 }
@@ -104,35 +107,52 @@ week_to_date <- function(week_n) {
 # Third plot
 color_hitos <- "firebrick3"
 
-casos_pred_median_testing %>% 
-  filter(Semana >= 30) %>% 
+casos_pred_10000_tests %>% 
+  filter(Semana >= 31) %>% 
   ggplot(aes(Semana, pred_with_median_tests)) + 
   geom_line() +
   geom_point() +
   geom_vline(xintercept = 33, color = color_hitos) +
   geom_vline(xintercept = 38, color = color_hitos) +
+  geom_vline(xintercept = 47.5, color = color_hitos) +
+  geom_vline(xintercept = 50.5, color = color_hitos) +
   annotate("label",
            x = 33,
-           y = 800,
+           y = 350,
            label = "Stgo. Centro avanzó\na fase 2", color = color_hitos) +
   annotate("label",
            x = 38,
-           y = 800,
+           y = 700,
            label = "Puente Alto avanzó\na fase 2", color = color_hitos) +
+  annotate("label",
+           x = 47.5,
+           y = 700,
+           label = "Providencia y Est. Central\navanzó a fase 4", color = color_hitos) +
+  annotate("label",
+           x = 50.5,
+           y = 700,
+           label = "RM retrocedió\na fase 2", color = color_hitos) +
   ggrepel::geom_text_repel(aes(label = scales::comma(pred_with_median_tests, 1)),
                            size = 3) +
   labs(title = "Casos diarios en Región Metropolitana\najustando por cantidad de tests PCR",
        subtitle = "Asumiendo 10.000 tests diarios",
        y = "Casos diarios",
        x = "Semana",
-       caption = "Datos: github.com/MinCiencia/Datos-COVID19.
+       caption = "Datos: github.com/MinCiencia/Datos-COVID19
        Código: github.com/franciscoyira/covid_plots") +
   scale_x_continuous(breaks = seq(30, 50, 2),
                      labels = week_to_date) +
   coord_flip() +
   ggthemes::theme_fivethirtyeight() +
   theme(plot.title.position = "plot",
-        plot.margin = unit(c(0.7,0.1,0.7,0.1), "inches")) +
+        # Whitespace in top and bottom to not overlap IG stories controls
+        plot.margin = unit(c(0.7,0.1,0.7,0.1), "inches"),
+        axis.title = element_text()) +
   ggsave(here("outputs", "casos_rm_stories.png"),
-         dpi = 400, width = 9, height = 16,
+         dpi = 400,
+         # IG stories aspect ratio
+         width = 9, height = 16,
          units = "cm", type = "cairo-png", scale = 1.4)
+
+# TODO: cambiar alineamiento de texto en histos (probar con justificado a
+# izquierda o derecha)
